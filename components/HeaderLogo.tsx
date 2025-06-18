0282a0a6ecb3d98e1843bd39363e4d437cd7b334
LB <lbutton@riacoding.net>
2025-06-18 08:56:32 -0700

    center logo

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -7,7 +7,7 @@ export default function HeaderLogo() {
   const { merchant } = usePublicMerchant()
   const logo = merchant?.s3ItemKey
   return (
-    <header className='min-h-32'>
+    <header className='flex flex-col items-center justify-center min-h-32'>
       {logo ? (
         <StorageImage width={128} className='h-32' path={`${logo}`} alt='logo' />
       ) : (
